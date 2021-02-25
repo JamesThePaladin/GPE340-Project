9d8dc510e8efa30123c1fb39b4ac9e3d95397b3c
JamesThePaladin <Jamesthepaladin@gmail.com>
2021-02-25 00:15:47 -0700

    Update Health.cs 2
    added events to methods and file

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,6 +5,12 @@ using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health Values")]
+    [SerializeField]
+    private float MaxHealth = 100f;
+    [SerializeField]
+    private float health = 100f;
+
     [Header("Events")]
     [SerializeField, Tooltip("Raised every time the object is healed.")]
     private UnityEvent onHeal;
@@ -12,9 +18,6 @@ public class Health : MonoBehaviour
     private UnityEvent onDamage;
     [SerializeField, Tooltip("Raised once when the object's health reaches 0.")]
     private UnityEvent onDeath;
-
-    [SerializeField] private static float MaxHealth = 100f;
-    [SerializeField] private float health = 100f;
 
     public float GetHealth() 
     {
@@ -39,19 +42,33 @@ public class Health : MonoBehaviour
     public void Heal(int heal) 
     {
         health += heal;
+        onHeal.Invoke();
+
+        if (health >= MaxHealth) 
+        {
+            health = MaxHealth;
+        }
     }
 
     public void FullHeal() 
     {
         health = MaxHealth;
     }
+
     public void Damage(int damage) 
     {
         health -= damage;
+        onDamage.Invoke();
+
+        if (health <= 0) 
+        {
+            health = 0;
+        }
     }
 
     public void Kill() 
     {
         health = 0;
+        onDeath.Invoke();
     }
 }

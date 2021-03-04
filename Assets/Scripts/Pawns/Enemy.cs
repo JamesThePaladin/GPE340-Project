255a54c7b264d0f4874e9d08d2c5cbe9d4d0d45b
JamesThePaladin <Jamesthepaladin@gmail.com>
2021-03-03 22:53:48 -0700

    Enemy Update
    Enemies now walk toward the player and have an actual enemy model.

--- Assets/Scripts/Pawns/Enemy.cs
+++ Assets/Scripts/Pawns/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Animations;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
@@ -10,15 +11,22 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent navMeshAgent;
     [SerializeField]
     private Transform target;
+    private Pawn pawn;
+    private Vector3 moveDirection;
     // Start is called before the first frame update
     void Start()
     {
+        pawn = GetComponent<Pawn>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        navMeshAgent.SetDestination(target.position);
+        moveDirection.x = target.position.x - transform.position.x;
+        moveDirection.y = target.position.y - transform.position.y;
+        moveDirection.z = target.position.z - transform.position.z;
+        transform.LookAt(target);
+        pawn.Move(moveDirection);
     }
 }
